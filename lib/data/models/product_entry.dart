31c4ce370e33ab9a8aeb98c0d18e6512e5070a2d
Hào Nguyễn <haonguyen@MacBook-Pro-cua-Hao.local>
2022-09-14 10:53:18 +0700

    update

--- lib/data/models/product_entry.dart
+++ lib/data/models/product_entry.dart
@@ -1,14 +1,8 @@
-import 'dart:convert';
-
 class ProductEntry {
   final int id;
-
   final String desc;
-
   final bool isAvailable;
-
   final double price;
-
   final List<String> types;
 
   ProductEntry({
@@ -19,32 +13,6 @@ class ProductEntry {
     this.types = const <String>[],
   });
 
-  ProductEntry copyWith({
-    int? id,
-    String? desc,
-    bool? isAvailable,
-    double? price,
-    List<String>? types,
-  }) {
-    return ProductEntry(
-      id: id ?? this.id,
-      desc: desc ?? this.desc,
-      isAvailable: isAvailable ?? this.isAvailable,
-      price: price ?? this.price,
-      types: types ?? this.types,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'desc': desc,
-      'isAvailable': isAvailable,
-      'price': price,
-      'types': types,
-    };
-  }
-
   factory ProductEntry.fromMap(Map<String, dynamic> map) {
     return ProductEntry(
       id: map['id']?.toInt() ?? 0,
@@ -53,12 +21,5 @@ class ProductEntry {
       price: map['price']?.toDouble() ?? 0.0,
       types: List<String>.from(map['types']),
     );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  @override
-  String toString() {
-    return toJson();
   }
 }
